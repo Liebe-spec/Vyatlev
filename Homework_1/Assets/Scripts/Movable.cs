b0f9c153f78acc80d62916a9ec751f4d24109aa9
Liebe-spec <yager.nik@mail.ru>
2025-05-18 18:11:08 +1000

    Add move for 3-d object

--- Homework_1/Assets/Scripts/Movable.cs
+++ Homework_1/Assets/Scripts/Movable.cs
@@ -1,21 +1,36 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UIElements;
 
 public class Movable : MonoBehaviour
 {
-    bool up; 
-
+    bool up;
+    int Temp;
     void Start()
     {
+        Temp = 1;
         Debug.Log("Start game");
     }
 
  
     void Update()
     {
+        
+        float y = GameObject.Find("Sphere").transform.position.y;
 
-        transform.Translate(0, 0.1f, 0);
-        transform.Rotate(0, 90 / 60f, 0); 
+        if (y > 4) Temp = 0;
+        else if (y < 3.5) Temp = 1;
+
+        if (Temp == 1)
+        {
+            transform.Translate(0, 0.001f, 0);
+        }
+        else if (Temp == 0)
+        {
+            transform.Translate(0, -0.001f, 0);
+        }
+
+
     }
 }
